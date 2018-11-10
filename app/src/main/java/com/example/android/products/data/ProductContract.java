baa8563ecc9c25acf6a20295af27b29c4a082077
Daniel Stanojevic <danielstanojevic@gmail.com>
2018-11-10 18:42:22 -0500

    Add Content Provider mime type and getType method

--- app/src/main/java/com/example/android/products/data/ProductContract.java
+++ app/src/main/java/com/example/android/products/data/ProductContract.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.products.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -59,6 +60,18 @@ public final class ProductContract {
 
         /** The content URI to access the pet data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCTS;
 
         /**
          * Name of database table for products
